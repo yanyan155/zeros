c3b690c12c06dab98bcabf22c4abace90d502587
yanyan155 <jm@tut.by>
2018-09-17 20:49:54 +0300

    assigment operator correction

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function getZerosCount(number) {
   // your implementation
   var count = 0;
   for (var i =0; i <= number; i = i +5) {
-  	for (var j = i; j%5 == 0 && j != 0; j = j/5) {
+  	for (var j = i; j%5 == 0 && j != 0; j /= 5) {
   		count++;
   	}
   }
